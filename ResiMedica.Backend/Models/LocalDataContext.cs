46cbb8256f04dcdf345ee327d4e5f1ab7412a1a8
barzahiero <ecanaleso.ing@gmail.com>
2019-09-29 23:44:51 -0500

    Para continuar

---
+++ ResiMedica.Backend/Models/LocalDataContext.cs
@@ -0,0 +1,8 @@
+﻿namespace ResiMedica.Backend.Models
+{
+    using Domain.Models;
+    public class LocalDataContext : DataContext
+    {
+        public System.Data.Entity.DbSet<ResiMedica.Common.Models.Product> Products { get; set; }
+    }
+}
---
+++ ResiMedica.Domain/Models/DataContext.cs
@@ -0,0 +1,13 @@
+﻿namespace ResiMedica.Domain.Models
+{
+    using System.Data.Entity;
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<ResiMedica.Common.Models.Product> Products { get; set; }
+    }
+}
